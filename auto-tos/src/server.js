65b7d1d39d096f32fd14545e910141756c3ce25a
ccficorilli <ccficorilli@gmail.com>
2018-09-03 13:15:53 -0700

    this code is working --needs to be cleaned up

--- auto-tos/src/server.js
+++ auto-tos/src/server.js
@@ -1,3 +1,5 @@
+    //?grant_type=authorization_code&refresh_token=&access_type=offline&code=${code}&client_id=${clientId}&redirect_uri=${redirectURI}
+
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
@@ -5,10 +7,17 @@ var request = require('request');
 var path = require('path');
 const axios = require('axios');
 const phrase = fs.readFileSync(path.resolve('../../ssl/phrase')).toString();
+const qs = require('qs');
+
+const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+}; 
+const client_id = 'TRADER_V4@AMER.OAUTHAP';
+const redirect_uri = 'https://localhost:443';
+const url = `https://api.tdameritrade.com/v1/oauth2/token/`;
+const form = 'form: '
 
 //SSL cert
-// var privateKey  = fs.readFileSync('../../ssl/server.key', 'utf8');
-// var certificate = fs.readFileSync('../../ssl/server.crt', 'utf8');
 var credentials = {
     key: fs.readFileSync(path.resolve('../../ssl/server.key')),
     cert: fs.readFileSync(path.resolve('../../ssl/server.crt')),
@@ -19,58 +28,60 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    const code = req.query.code; //get the code
-    console.log(code);
-    const url = 'https://api.tdameritrade.com/v1/oauth2/token/';
-    
-    axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-    axios.post({
-        method: 'post',
-        headers: 'application/x-www-form-urlencoded',
-        url: url,
-        data: {
-            'grant_type': 'authorization_code',
-            'refresh_token': '',
-            'access_type': 'offline',
-            'code': code, 
-            'client_id': 'TRADER_V4@AMER.OAUTHAP',
-            'redirect_uri': 'https://localhost:443'
-            }
-        }).then(response => {
-            console.log(response.data);
-            res.send(response.data);
-        }).catch(err => console.log(err));
-        // 	var options = {
-//                 //see the Authentication API's Post Access Token method for more information
-// 		url: 'https://api.tdameritrade.com/v1/oauth2/token',
-// 		method: 'POST',
-// 		headers: headers,
-//                 //POST Body params
-// 		form: {
-// 			'grant_type': 'authorization_code',
-// 			'access_type': 'offline',
-// 			'code': req.query.code, //get the code
-// 			'client_id': 'TRADER_V2%40AMER.OAUTHAP',
-// 			'redirect_uri': 'https%3A%2F%2Flocalhost%3A443'
-// 		}
-// 	}
+    // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+    const code = req.query.code.toString(); //get the code
+    // console.log(code);
+    // axios({
+    //     method: 'POST',
+    //     url: 'https://api.tdameritrade.com/v1/oauth2/token',
+	// 	headers: headers,
+    //             //POST Body params
+	// 	form: {
+	// 		grant_type: 'authorization_code',
+	// 		access_type: 'offline',
+	// 		code: code,
+	// 		client_id: 'TRADER_V4%40AMER.OAUTHAP',
+	// 		redirect_uri: 'https%3A%2F%2Flocalhost%3A443'
+	// 	}
+    // })
+    // .then(rs => res.send(rs.data))
+    // .catch(err => console.log(err))
+
+
+
+    // res.send('Everything should be working now');
+
+        	var options = {
+                //see the Authentication API's Post Access Token method for more information
+		url: 'https://api.tdameritrade.com/v1/oauth2/token',
+		method: 'POST',
+		// headers: headers,
+                //POST Body params
+		form: {
+			'grant_type': 'authorization_code',
+			'access_type': 'offline',
+			'code': req.query.code, //get the code
+			'client_id': 'TRADER_V4@AMER.OAUTHAP',
+			'redirect_uri': 'https://localhost:443'
+		}
+	}
         
-//         //Post Access Token request
-// 	request(options, function(error, response, body) {
-// 		if (!error && response.statusCode == 200) {
-// 			//see Post Access Token response summary for what authReply contains
-//             authReply = JSON.parse(body);
-//             console.log(authReply);
+        //Post Access Token request
+	request(options, function(error, response, body) {
+		//see Post Access Token response summary for what authReply contains
+            authReply = JSON.parse(body);
+            // authResponse = JSON.parse(response);
+            console.log({error:error},{response:response},{body:body});
 			
-// 			//the line below is for convenience to test that it's working after authenticating
-// 			res.send(authReply);
-// 		}
-// 	})
+			//the line below is for convenience to test that it's working after authenticating
+			res.send(authReply);
+		
+	})
 	
-// 	function errorHandler (err, req, res, next) {
-// 		res.status(500)
-// 		res.render('error', { error: err })
-// }
+	// function errorHandler (err, req, res, next) {
+	// 	res.status(500)
+	// 	res.render('error', { error: err })
+//}
 });
 
 var httpServer = http.createServer(app);
