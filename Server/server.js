abba1b9e9954a88258f33ad6fbe7ed2b652c84ed
ccficorilli <ccficorilli@gmail.com>
2018-09-03 19:35:02 -0700

    cleaned up some more --need to make more secure

--- Server/server.js
+++ Server/server.js
@@ -1,17 +1,19 @@
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
-const request = require('request');
-const path = require('path');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var request = require('request');
+var path = require('path');
+// const axios = require('axios');
 const phrase = fs.readFileSync(path.resolve('./ssl/phrase')).toString();
+// const qs = require('qs');
 const express = require('express');
-const app = express();
-//const axios = require('axios');
-//const qs = require('qs');
 
+const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+}; 
 const client_id = 'TRADER_V4@AMER.OAUTHAP';
 const redirect_uri = 'https://localhost:443';
-
+const url = `https://api.tdameritrade.com/v1/oauth2/token/`;
 
 //SSL cert
 const credentials = {
@@ -20,16 +22,15 @@ const credentials = {
     passphrase: phrase
 };
 
+const app = express();
+
 app.get('/', function(req, res){
 	const code = req.query.code;
-	console.log(code);
     const options = {
                 //see the Authentication API's Post Access Token method for more information
 		url: 'https://api.tdameritrade.com/v1/oauth2/token',
 		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
+		headers: headers,
                 //POST Body params
 		form: {
 			'grant_type': 'authorization_code',
@@ -43,27 +44,27 @@ app.get('/', function(req, res){
         //Post Access Token request
 	request(options, function(error, response, body) {
 		//see Post Access Token response summary for what authReply contains
-			authReply = JSON.parse(body);
+			const authReply = JSON.parse(body);
 			fs.writeFile('./ssl/access_token.json',JSON.stringify({
 				access_token: authReply.access_token,
 				refresh_token: authReply.refresh_token,
 				access_expires_by: Date.now() + (authReply.expires_in * 1000),
 				refresh_expires_by: Date.now() + (authReply.refresh_token_expires_in * 1000)
 			}),()=> console.log('access API written OK'));
-
+			            			
 			//the line below is for convenience to test that it's working after authenticating
 			res.send(authReply);
 		
 	})
-	//Need to write an error handler....
+	
 	// function errorHandler (err, req, res, next) {
 	// 	res.status(500)
 	// 	res.render('error', { error: err })
 //}
 });
 
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
 //Set to 8080, but can be any port, code will only come over https, even if you specified http in your Redirect URI
 httpServer.listen(8080, console.log('server is on 8080'));
